4a5a1406a3065eb98d4323ea538545001872e780
Simon Pirko <sipi7gm@gmail.com>
2019-11-09 23:12:03 +0300

    init

---
+++ src/main/java/by/numberb/numberbv1/NumberbV1Application.java
@@ -0,0 +1,13 @@
+package by.numberb.numberbv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberbV1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NumberbV1Application.class, args);
+  }
+
+}
